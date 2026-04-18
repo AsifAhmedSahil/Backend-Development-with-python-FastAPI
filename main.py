eda46b9d7fc1a6d35daab5f2bf57bedd6214d308
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2026-04-18 12:55:46 +0600

    Path Parameters & Query Parameters in FastAPI

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # print(fastapi.__version__)
 
-from fastapi import FastAPI 
+from fastapi import FastAPI ,Request
+from mockData import products
 
 app = FastAPI()
 
@@ -14,3 +15,38 @@ def home():
 def contact():
     return "contact us!"
 
+@app.get("/products")
+def get_products():
+    return products
+
+# path params
+
+@app.get("/product/{product_id}")
+def get_one_product(product_id:int):
+    for oneProduct in products:
+        if oneProduct.get("id") == product_id:
+            return oneProduct
+
+    return {
+        "error":"Product not found in database"
+    }
+
+
+# Query parameter 
+
+# @app.get("/greet")
+# def greet(name:str,age:int):
+#     return {
+#         "greet":f"Hello {name}. Your age is {age}"
+#     }
+
+# Query parameter with all request in one
+
+@app.get("/greetuser")
+def greet_user(request:Request):
+    
+    query_params = dict(request.query_params)
+    print(query_params)
+    return {
+        "greet":f"Hello {query_params.get("name")}. Your age is {query_params.get("age")}"
+    }
---
+++ mockData.py
@@ -0,0 +1,32 @@
+products = [
+  {
+    "id": 1,
+    "title": "Laptop",
+    "count": 10,
+    "price": 75000
+  },
+  {
+    "id": 2,
+    "title": "Smartphone",
+    "count": 25,
+    "price": 30000
+  },
+  {
+    "id": 3,
+    "title": "Headphones",
+    "count": 15,
+    "price": 12000
+  },
+  {
+    "id": 4,
+    "title": "Monitor",
+    "count": 8,
+    "price": 20000
+  },
+  {
+    "id": 5,
+    "title": "Keyboard",
+    "count": 20,
+    "price": 2500
+  }
+]
