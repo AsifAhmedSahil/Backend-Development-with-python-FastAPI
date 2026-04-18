cc4d86b00ed22f24f21cb14cbd2100ff15e52a53
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2026-04-18 11:48:58 +0600

    FastAPI Server & GET Endpoint

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import fastapi
+# import fastapi
 
-print(fastapi.__version__)
+# print(fastapi.__version__)
+
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Welcome Home"
+
+@app.get("/contact")
+def contact():
+    return "contact us!"
+
